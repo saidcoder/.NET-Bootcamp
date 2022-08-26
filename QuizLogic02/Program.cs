ebeccd970a61e387cb2be1bfb5e1df2348ba8978
saidcoder <saidgrmc16@gmail.com>
2022-08-26 15:14:56 +0700

    commit project
    commit project

---
+++ QuizLogic02/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+using QuizLogic02;
+
+/*var myRandom = day02.RandomArray(6);
+day02.DisplayArray(myRandom);
+
+WriteLine();
+
+var myRandomIndex = day02.RandomIndex(myRandom);
+day02.DisplayArray(myRandomIndex);*/
+
+//==============NO 2==================
+var myArray = new int[] { 15, 2, 30, 12, 10, 5 };
+Console.WriteLine("Before Shifting");
+/*day02.DisplayArray(myArray);
+WriteLine();
+Console.WriteLine("After Shifting");
+day02.DisplayArray(day02.SwitchArray(myArray));*/
+//======================================
+Console.WriteLine();
+//================No 3================
+day02.RotateArray(myArray);
+
+//no.4
+//day02.RotateRightArray(myArray);
+
+//no 7
+//day02.DisplayArray(day02.DuplicateArray());
+
+ReadLine();
---
+++ QuizLogic02/day02.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizLogic02
+{
+    internal class day02
+    {
+        //01
+        public static int[] RandomArray(int n)
+        {
+            int[] arr = new int[n];
+            Random rand = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rand.Next(50);
+            }
+            Console.WriteLine($"Before Random");
+            return arr;
+        }
+        public static int[] RandomIndex(int[] arr)
+        {
+            Random rand = new Random();
+            //int[] arrRandom = new int[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int temp = arr[i];
+                int j = rand.Next(0, arr.Length);
+                arr[i] = arr[j];
+                arr[j] = temp;
+
+            }
+            Console.WriteLine($"After Random");
+
+            return arr;
+        }
+
+        //no.2
+        public static int[] SwitchArray(int[] arr)
+        {
+            int[] arrRotate = new int[arr.Length];
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                arrRotate[i] = arr[i + 1];
+            }
+            arrRotate[arr.Length - 1] = arr[0];
+            return arrRotate;
+        }
+
+        //no.3
+        public static int[] RotateArray(int[] arr)
+        {
+            int[] arrRotate = new int[arr.Length];
+            Console.WriteLine($"Before Rotate :");
+            DisplayArray(arr);
+
+            for (int j = 1; j <= 3; j++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Rotate ke {j} :");
+                int last = arr[0];
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    arrRotate[i] = arr[i + 1];
+
+                }
+                arrRotate[arr.Length - 1] = last;
+                DisplayArray(arrRotate);
+                arr = arrRotate;
+            }
+            return arrRotate;
+        }
+
+        //no.4
+        public static int[] RotateRightArray(int[] arr)
+        {
+            int[] arrRotate = new int[arr.Length];
+            Console.WriteLine($"Before Rotate :");
+            DisplayArray(arr);
+
+            for (int j = 1; j <= 3; j++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Rotate ke {j} :");
+                int last = arr[arr.Length - 1];
+                for (int i = arr.Length - 1 ; i > 0;  i--)
+                {
+                    arrRotate[i] = arr[i - 1];
+
+                }
+                arrRotate[0] = last;
+                DisplayArray(arrRotate);
+                arr = arrRotate;
+            }
+            return arrRotate;
+        }
+
+        //no 7
+        public static int[] DuplicateArray()
+        {
+            var arr = new[] { 1, 2, 3, 2, 7, 7, 1, 6, 3, 4, 5, 2, 3, 6, 8, 9 };
+            Console.WriteLine("Before Remove");
+            DisplayArray(arr);
+            Console.WriteLine();
+            var uniqueArray = arr.Distinct().ToArray();
+            Console.WriteLine("After Remove");
+            Array.Sort(uniqueArray);
+            return uniqueArray;
+        }
+
+        public static void DisplayArray(int[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+    }
+}
